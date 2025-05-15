9b2a03d29180f102c66f7bd78d2ce185df68cb49
wasfy <eng.omarwasfy@gmail.com>
2025-05-15 20:02:41 +0300

    fixes

--- fundus/segment_anything/modeling/adapter/Adapter.py
+++ fundus/segment_anything/modeling/adapter/Adapter.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 
@@ -8,26 +7,31 @@ from segment_anything.modeling.adapter.Filter import ChannelFilter
 
 
 class Adapter(nn.Module):
-    def __init__(self, D_features, mlp_ratio=0.25, act_layer=nn.GELU, skip_connect=True):
+    def __init__(self, D_features, mlp_ratio=0.25, act_layer=nn.GELU, skip_connect=True, num_layers=10):
         super().__init__()
         self.channel_filter = ChannelFilter()
-
         self.skip_connect = skip_connect
         D_hidden_features = int(D_features * mlp_ratio)
         self.act = act_layer()
-        self.D_fc1 = nn.Linear(D_features, D_hidden_features)
-        self.D_fc2 = nn.Linear(D_hidden_features, D_features)
+        
+        # Create 10 layers of MLP blocks
+        self.layers = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(D_features, D_hidden_features),
+                act_layer(),
+                nn.Linear(D_hidden_features, D_features)
+            ) for _ in range(num_layers)
+        ])
 
-    def forward(self, x, embedding_feature):#
+    def forward(self, x, embedding_feature):
         x = x + embedding_feature.permute(0, 2, 3, 1)
         x = self.channel_filter(x)
 
-        # x is (BT, HW+1, D)
-        xs = self.D_fc1(x)
-        xs = self.act(xs)
-        xs = self.D_fc2(xs)
-        if self.skip_connect:
-            x = x + xs
-        else:
-            x = xs
+        # Apply all layers sequentially with skip connections
+        for layer in self.layers:
+            if self.skip_connect:
+                x = x + layer(x)
+            else:
+                x = layer(x)
+        
         return x
--- prostate/segment_anything/modeling/adapter/Adapter.py
+++ prostate/segment_anything/modeling/adapter/Adapter.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 
@@ -8,30 +7,31 @@ from segment_anything.modeling.adapter.Filter import ChannelFilter
 
 
 class Adapter(nn.Module):
-    def __init__(self, D_features, mlp_ratio=1.0, act_layer=nn.GELU, skip_connect=True):
+    def __init__(self, D_features, mlp_ratio=0.25, act_layer=nn.GELU, skip_connect=True, num_layers=1):
         super().__init__()
         self.channel_filter = ChannelFilter()
-
         self.skip_connect = skip_connect
         D_hidden_features = int(D_features * mlp_ratio)
         self.act = act_layer()
-        # Five-layer MLP
-        self.D_fc1 = nn.Linear(D_features, D_hidden_features)
-        self.D_fc2 = nn.Linear(D_hidden_features, D_hidden_features)
-        self.D_fc3 = nn.Linear(D_hidden_features, D_hidden_features)
-        self.D_fc4 = nn.Linear(D_hidden_features, D_hidden_features)
-        self.D_fc5 = nn.Linear(D_hidden_features, D_features)
+        
+        # Create 10 layers of MLP blocks
+        self.layers = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(D_features, D_hidden_features),
+                act_layer(),
+                nn.Linear(D_hidden_features, D_features)
+            ) for _ in range(num_layers)
+        ])
 
-    def forward(self, x, embedding_feature):#
-        # Note: The line below was causing a RuntimeError due to dimension mismatch.
-        # If embedding_feature is intended to be used, its integration should be handled differently.
+    def forward(self, x, embedding_feature):
         x = x + embedding_feature.permute(0, 2, 3, 1)
-        x_filtered = self.channel_filter(x)
+        x = self.channel_filter(x)
 
-        # Five-layer MLP calculation
-        mlp_output = self.D_fc5(self.act(self.D_fc4(self.act(self.D_fc3(self.act(self.D_fc2(self.act(self.D_fc1(x_filtered)))))))))
-        if self.skip_connect:
-            output = x_filtered + mlp_output  # Skip connection with filtered features
-        else:
-            output = mlp_output
-        return output
+        # Apply all layers sequentially with skip connections
+        for layer in self.layers:
+            if self.skip_connect:
+                x = x + layer(x)
+            else:
+                x = layer(x)
+        
+        return x
