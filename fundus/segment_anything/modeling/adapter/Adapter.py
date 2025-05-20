880862b7c7d5a0e9b9a7d56c64e50357ec591da2
wasfy <eng.omarwasfy@gmail.com>
2025-05-20 20:02:52 +0300

    fixes

--- fundus/segment_anything/modeling/adapter/Adapter.py
+++ fundus/segment_anything/modeling/adapter/Adapter.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 
@@ -8,7 +7,7 @@ from segment_anything.modeling.adapter.Filter import ChannelFilter
 
 
 class Adapter(nn.Module):
-    def __init__(self, D_features, mlp_ratio=0.25, act_layer=nn.GELU, skip_connect=True):
+    def __init__(self, D_features, mlp_ratio=0.25, act_layer=nn.GELU, skip_connect=True, dropout_rate=0.1, num_attention_heads=8):
         super().__init__()
         self.channel_filter = ChannelFilter()
 
@@ -17,14 +16,26 @@ class Adapter(nn.Module):
         self.act = act_layer()
         self.D_fc1 = nn.Linear(D_features, D_hidden_features)
         self.D_fc2 = nn.Linear(D_hidden_features, D_features)
+        self.norm = nn.LayerNorm(D_features)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.attn = nn.MultiheadAttention(D_features, num_attention_heads, batch_first=True)
 
     def forward(self, x, embedding_feature):#
         x = x + embedding_feature.permute(0, 2, 3, 1)
         x = self.channel_filter(x)
 
         # x is (BT, HW+1, D)
+        # Apply self-attention
+        B, H, W, D = x.shape
+        attn_input = x.reshape(B, H*W, D)
+        attn_output, _ = self.attn(attn_input, attn_input, attn_input)
+        x = attn_output.reshape(B, H, W, D)
+
+        # LayerNorm before MLP
+        x = self.norm(x)
         xs = self.D_fc1(x)
         xs = self.act(xs)
+        xs = self.dropout(xs)
         xs = self.D_fc2(xs)
         if self.skip_connect:
             x = x + xs
