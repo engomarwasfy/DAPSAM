dfce49fca2f7b442f130cc8392a2c2d09c0a1001
wasfy <eng.omarwasfy@gmail.com>
2025-05-21 13:30:45 +0300

    fixes

--- prostate/segment_anything/modeling/adapter/Adapter.py
+++ prostate/segment_anything/modeling/adapter/Adapter.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 
@@ -8,7 +7,7 @@ from segment_anything.modeling.adapter.Filter import ChannelFilter
 
 
 class Adapter(nn.Module):
-    def __init__(self, D_features, mlp_ratio=0.25, act_layer=nn.GELU, skip_connect=True):
+    def __init__(self, D_features, mlp_ratio=0.25, act_layer=nn.GELU, skip_connect=True, dropout_rate=0.1):
         super().__init__()
         self.channel_filter = ChannelFilter()
 
@@ -17,6 +16,8 @@ class Adapter(nn.Module):
         self.act = act_layer()
         self.D_fc1 = nn.Linear(D_features, D_hidden_features)
         self.D_fc2 = nn.Linear(D_hidden_features, D_features)
+        self.residual_scale = nn.Parameter(torch.ones(1))
+        self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, x, embedding_feature):#
         x = x + embedding_feature.permute(0, 2, 3, 1)
@@ -25,9 +26,10 @@ class Adapter(nn.Module):
         # x is (BT, HW+1, D)
         xs = self.D_fc1(x)
         xs = self.act(xs)
+        xs = self.dropout(xs)  # Add dropout after activation
         xs = self.D_fc2(xs)
         if self.skip_connect:
-            x = x + xs
+            x = x + self.residual_scale * xs
         else:
             x = xs
         return x
